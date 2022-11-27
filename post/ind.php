a2e86de0c37cf74872f9437e39aaddd99252ff95
TRohan007 <116061395+TRohan007@users.noreply.github.com>
2022-11-27 11:13:28 +0530

    Add files via upload

---
+++ post/ind.php
@@ -0,0 +1,109 @@
+<?php
+error_reporting(0);
+
+$msg = "";
+
+// If upload button is clicked ...
+if (isset($_POST['upload'])) {
+
+	$filename = $_FILES["uploadfile"]["name"];
+	$tempname = $_FILES["uploadfile"]["tmp_name"];
+	$folder = "./image/" . $filename;
+
+	$db = mysqli_connect("localhost", "root", "", "geeksforgeeks");
+
+	// Get all the submitted data from the form
+	$sql = "INSERT INTO image (filename) VALUES ('$filename')";
+
+	// Execute query
+	mysqli_query($db, $sql);
+
+	// Now let's move the uploaded image into the folder: image
+	if (move_uploaded_file($tempname, $folder)) {
+		echo "<h3> Image uploaded successfully!</h3>";
+	} 
+    else {
+		echo "<h3>  upload image!</h3>";
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Image Upload</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<style>
+        *{
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+#content{
+	width: 50%;
+	justify-content: center;
+	align-items: center;
+	margin: 20px auto;
+	border: 1px solid #cbcbcb;
+}
+form{
+	width: 50%;
+	margin: 20px auto;
+}
+
+#display-image{
+	width: 100%;
+	justify-content: center;
+	padding: 5px;
+	margin: 15px;
+}
+img{
+	margin: 5px;
+	width: 350px;
+	height: 250px;
+}
+
+    </style>
+</head>
+
+<body>
+	<div id="content">
+		<form method="POST" action="" enctype="multipart/form-data">
+			<div class="form-group">
+				<input class="form-control" type="file" name="uploadfile" value="" />
+			</div>
+			<div class="form-group">
+				<button class="btn btn-primary" type="submit" name="upload">UPLOAD</button>
+			</div>
+            <div>
+               Location <br><input type="text" name="location" id="loc">
+            </div>
+            <div>
+                <div>
+                    <br>
+                </div>
+               Date <br ><input type="datetime-local" name="date" id="dat">
+            </div>
+            <br><br>
+            Description
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+		</form>
+	</div>
+	<div id="display-image">
+		<?php
+		$query = " select * from image ";
+		$result = mysqli_query($db, $query);
+
+		while ($data = mysqli_fetch_assoc($result)) {
+		?>
+			<img src="./image/<?php echo $data['filename']; ?>">
+
+		<?php
+		}
+		?>
+	</div>
+</body>
+
+</html>
